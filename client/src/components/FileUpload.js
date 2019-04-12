e73d6a4cf74e0139ab7edea99565842aa7fe98f0
sajicode <sajioloye@gmail.com>
2019-04-12 11:41:48 +0100

    upload and display file

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,13 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import axios from "axios";
 
 const FileUpload = () => {
+  const [file, setFile] = useState("");
+  const [filename, setFilename] = useState("Choose File");
+  const [uploadedFile, setUploadedFile] = useState({});
+
+  const onChange = e => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+    } catch (error) {
+      if (error.response.status === 500)
+        console.log("There was a server problem");
+      else console.log(error.response.data.msg);
+    }
+  };
+
   return (
     <Fragment>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="custom-file mb-4">
-          <input type="file" className="custom-file-input" id="customFile" />
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={onChange}
+          />
           <label className="custom-file-label" htmlFor="customFile">
-            Choose File
+            {filename}
           </label>
         </div>
 
@@ -17,6 +55,19 @@ const FileUpload = () => {
           className="btn btn-primary btn-block mt-4"
         />
       </form>
+
+      {uploadedFile && (
+        <div className="row mt-5">
+          <div className="col-md-6 m-auto">
+            <h3 className="text-center">{uploadedFile.fileName}</h3>
+            <img
+              src={uploadedFile.filePath}
+              alt="uploaded pic"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+      )}
     </Fragment>
   );
 };
